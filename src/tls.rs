fe303638a790b38815c3c741cf22fcf531ce7ede
Martin Garton <garton@gmail.com>
2022-01-17 16:26:13 +0000

    Rename TLS to Tls
    Rename TLS to Tls to silence clippy.

--- src/tls.rs
+++ src/tls.rs
@@ -17,14 +17,14 @@ pub(crate) struct SwitchableConn<T: Read + Write>(Option<EitherConn<T>>);
 
 pub(crate) enum EitherConn<T: Read + Write> {
     Plain(T),
-    TLS(rustls::StreamOwned<ServerConnection, T>),
+    Tls(rustls::StreamOwned<ServerConnection, T>),
 }
 
 impl<T: Read + Write> Read for SwitchableConn<T> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         match &mut self.0.as_mut().unwrap() {
             EitherConn::Plain(p) => p.read(buf),
-            EitherConn::TLS(t) => t.read(buf),
+            EitherConn::Tls(t) => t.read(buf),
         }
     }
 }
@@ -33,14 +33,14 @@ impl<T: Read + Write> Write for SwitchableConn<T> {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         match &mut self.0.as_mut().unwrap() {
             EitherConn::Plain(p) => p.write(buf),
-            EitherConn::TLS(t) => t.write(buf),
+            EitherConn::Tls(t) => t.write(buf),
         }
     }
 
     fn flush(&mut self) -> io::Result<()> {
         match &mut self.0.as_mut().unwrap() {
             EitherConn::Plain(p) => p.flush(),
-            EitherConn::TLS(t) => t.flush(),
+            EitherConn::Tls(t) => t.flush(),
         }
     }
 }
@@ -52,8 +52,8 @@ impl<T: Read + Write> SwitchableConn<T> {
 
     pub fn switch_to_tls(&mut self, config: Arc<ServerConfig>) -> io::Result<()> {
         let replacement = match self.0.take() {
-            Some(EitherConn::Plain(plain)) => Ok(EitherConn::TLS(create_stream(plain, config)?)),
-            Some(EitherConn::TLS(_)) => Err(io::Error::new(
+            Some(EitherConn::Plain(plain)) => Ok(EitherConn::Tls(create_stream(plain, config)?)),
+            Some(EitherConn::Tls(_)) => Err(io::Error::new(
                 io::ErrorKind::Other,
                 "tls variant found when plain was expected",
             )),
